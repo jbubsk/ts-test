22888b54e8d1865b3705d8b06190679c0009a70d
Mikhail Zheludev <develojbu@gmail.com>
2016-02-22 19:29:05 +0300

    added config for running webpack-dev-server

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 funny-start
+with angular
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-    entry: ''
+    entry: './static/src/start',
+    output: {
+        path: 'build',
+        filename: 'bundle.js',
+        publicPath: '/assets/'
+    },
+    resolve: {
+        root: __dirname + '/node_modules',
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    resolveLoader: {
+        root: __dirname + '/node_modules',
+        modulesDirectories: ['/node_modules']
+
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    }
 };
